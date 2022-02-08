db342310072e425b39c4045152481f924b85a55e
Mehul Oswal <mehuloswal21@gmail.com>
2022-02-09 00:41:34 +0530

    Modified Tests

--- mygifcollection/tests/mygifcollection.js
+++ mygifcollection/tests/mygifcollection.js
@@ -1,28 +1,45 @@
-const assert = require("assert");
 const anchor = require("@project-serum/anchor");
-const { SystemProgram } = require("@solana/web3.js");
 
-describe("mygifcollection", () => {
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
+
   const program = anchor.workspace.Mygifcollection;
+
+  // Create an account keypair for our program to use.
   const baseAccount = anchor.web3.Keypair.generate();
 
-  console.log(anchor.Provider.wallet);
-
-  it("Is initialized!", async () => {
-    await program.rpc.initialize({
-      accounts: {
-        baseAccount: baseAccount.publicKey,
-        user: anchor.Provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [baseAccount],
-    });
-
-    const account = await program.account.baseAccount.fetch(
-      baseAccount.publicKey
-    );
-    assert.equal("0", account.totalGifs.toString(), "They are not equal");
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
